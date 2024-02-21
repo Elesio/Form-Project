e87ff50afa4cb6f028412794bc67737a8d028c5c
Elesio <160021368+Elesio@users.noreply.github.com>
2024-02-21 13:36:24 +0100

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
-from flask_login import UserMixin
-import psycopg2
+from flask import jsonify
+from flask import send_file
+import shutil
+'''from flask_login import UserMixin
+import psycopg2'''
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/postgres'
@@ -31,15 +35,45 @@ def login():
 def loginnow():
     return render_template('form.html')
 
+@app.route("/<string:filename>")
+def givefile(filename):
+    return send_file(filename)
+
 @app.route('/login',methods=['GET','POST'])
 def pyserv():
     name = 'Zin'
     if request.method == 'POST':
-        name = request.form['name']
-        return 'Hello ' + name + '!'
+        newsave = []
+        #hier rein: Die Werteumwandeln in eine Datei
+        filepath = app.root_path + '\myfile.txt'
+        if os.path.exists(filepath):
+            os.remove(filepath)
+        with open(filepath, 'a') as fo:
+            #fo.write("This is Test Data")
+            for item in request.json:
+                newlist = []
+                if len(item)!=len(request.json[0]):
+                    for thing in item:
+                        newlist.append(thing)
+                    for l in range(len(request.json[0])-len(item)):
+                        newlist.append('')
+                else:
+                    for thing in item:
+                        newlist.append(thing)
+                newsave.append(newlist)
+            for i in range(len(newsave)):
+                for ii in range(len(newsave[0])):
+                    if ii != len(newsave[0])-1 and newsave[i][ii+1]!='':
+                        fo.write("%s," % newsave[i][ii])
+                    elif ii == len(newsave[0])-1 or newsave[i][ii+1]=='':
+                        fo.write("%s" % newsave[i][ii])
+                        fo.write('\n')
+            fo.close()
+        #return request.json
+            return filepath
     else:
         print('get')
-        name = request.args.get('name')
+        name = 'Noah'
         return 'Tschüss ' + name + '!'
 
 if __name__ == "__main__":
