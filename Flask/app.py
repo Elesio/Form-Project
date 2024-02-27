f27eeef1c6da84f16d21c2f428e15a61b399fe0e
Elesio <160021368+Elesio@users.noreply.github.com>
2024-02-27 09:08:15 +0100

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -6,6 +6,44 @@ from flask import jsonify
 from flask import send_file
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 import os
+from io import StringIO, BytesIO
+import psycopg2
+
+conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="natlanmap39", port=5432)
+
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    isadmin VARCHAR(255)
+);
+""")
+
+tid = 5
+tname = 'FabulousFox'
+tpass = 'xx_superduperpasswordextreme_xx'
+tadmin = 'maybe'
+
+cur.execute("""INSERT INTO users (id, username, password, isadmin) VALUES
+(1, 'Elesio', 'goodpassword', 'y'),
+(2, 'Nadoriana', 'vergoodpassword', 'y'),
+(3, 'Supefredi', 'passwordmanager', 'y'),
+(4, 'Antiunkraut', 'password', 'n');
+""")
+
+sql = cur.mogrify("""INSERT INTO users (id, username, password, isadmin) VALUES (%s, %s, %s, %s);""", (tid,tname,tpass,tadmin))
+cur.execute(sql)
+
+cur.execute("""SELECT * FROM users""")
+
+print(cur.fetchall())
+
+conn.commit()
+
+cur.close()
+conn.close()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databank.db'
@@ -24,7 +62,7 @@ with app.app_context():
 def adduser():
     return render_template("adduser.html")
 
-@app.route("/conf",methods=['GET','POST'])
+@app.route("/conf",methods=['POST'])
 def pagestart():
     if request.method == 'POST':
         return render_template('index.html')
@@ -35,7 +73,7 @@ def pagestart():
 def list():
     return render_template('list.html')
 
-@app.route("/files",methods=['GET','POST'])
+@app.route("/files",methods=['POST'])
 def files():
     if request.method == 'POST':
         file = request.files['file']
@@ -45,12 +83,38 @@ def files():
         db.session.commit()
 
         return f'Uploaded: {file.filename}'
+    
+@app.route('/d',methods=['POST'])
+def download():
+    start = 0
+    end = 0
+    for i in range(100):
+        try:
+            upload = Upload.query.filter_by(id=i).first()
+            f = BytesIO(upload.data)
+            start = start + 1
+        except:
+            upload = Upload.query.filter_by(id=i).first()
+            end = end + 1
+    
+    filepath = app.root_path + '\myfile.txt'
+    upload = Upload.query.filter_by(id=start).first()
+    bytedata = BytesIO(upload.data)
+
+    b = bytedata.read()
+    
+    with open(filepath, 'a') as fo:
+        fo.write("%sstart is " % start)
+        fo.write("%s and end is " % end)
+        fo.write(str(type(bytedata)))
+    
+    return send_file(BytesIO(upload.data), download_name=upload.filename, as_attachment=True)
 
 @app.route("/")
 def login():
     return render_template('login.html')
 
-@app.route("/form")
+@app.route("/form",methods=['POST'])
 def form():
     return render_template('new.html')
 
