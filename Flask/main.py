9fa9f307638b18f82a29ef7a6f91f6c4f3890221
Elesio <160021368+Elesio@users.noreply.github.com>
2024-03-08 10:13:47 +0100

    Delete Flask/main.py

--- Flask/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:natlanmap39@localhost/students'
-
-db=SQLAlchemy(app)
-#https://www.youtube.com/watch?v=XZ_gAWdGzZk
